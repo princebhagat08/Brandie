a596f2efee443624c49895b75ee256613d963f65
Prince Kumar <prince.kumar@4thpointer.com>
2026-07-11 01:31:26 +0530

    fix orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:brandie/app/constant/theme/app_theme.dart';
 import 'package:brandie/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
 
